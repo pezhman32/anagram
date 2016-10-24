f0c3c70d533b0dfbf13645d3e27fd1d0963a35fc
Pezhman  Jahanmard <pezhman32@gmail.com>
2016-10-24 21:22:02 +0330

    Executable maven files added to project

---
+++ src/main/java/com/kaufland/exercise/Application.java
@@ -0,0 +1,13 @@
+package com.kaufland.exercise;
+
+/**
+ * Main class to start our application
+ *
+ * @author Pezhman Jahanmard
+ */
+public class Application {
+
+	public static void main(String[] args) {
+		System.out.println("Some output");
+	}
+}
